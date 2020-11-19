354ce80b67a92e0478c45cf8f96f144d5f7fff86
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2020-11-19 23:48:48 +0200

    password reset and more refactor

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -1,14 +1,30 @@
-  
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
+from blogapp.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
-from blogapp import routes
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from blogapp.main.routes import main
+    from blogapp.users.routes import users
+    from blogapp.posts.routes import posts
+
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+
+    return app
